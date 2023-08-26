60abe004d3915c64a4de9b83c016696d2f83ab32
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-26 06:29:36 +0100

    feat: create the scope in the chapter 98
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/main/java/org/a4j/workshop/helidon/cdi/news/SocialMedia.java
+++ chapter-09/src/main/java/org/a4j/workshop/helidon/cdi/news/SocialMedia.java
@@ -1,9 +1,11 @@
 package org.a4j.workshop.helidon.cdi.news;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 
+@ApplicationScoped
 public class SocialMedia implements Consumer<News> {
 
     private static final Logger LOGGER = Logger.getLogger(SocialMedia.class.getName());
