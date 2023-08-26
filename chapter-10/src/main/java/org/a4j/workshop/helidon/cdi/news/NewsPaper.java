bf15130ce035c529e38dd0ef3c8b03809b3f8c08
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-26 07:02:58 +0100

    create chapter 10
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-10/src/main/java/org/a4j/workshop/helidon/cdi/news/NewsPaper.java
@@ -0,0 +1,17 @@
+package org.a4j.workshop.helidon.cdi.news;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class NewsPaper implements Consumer<News> {
+
+    private static final Logger LOGGER = Logger.getLogger(NewsPaper.class.getName());
+
+    @Override
+    public void accept(@Observes News news) {
+        LOGGER.info("We got the news, we'll publish it on a newspaper: " + news.get());
+    }
+}
